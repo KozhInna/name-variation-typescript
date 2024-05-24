fc0641f91973c8841557e16f2e70b3cf54129e4e
Inna Kozhina <inna.kozhina22@gmail.com>
2024-05-24 22:11:20 +0300

    variation function implementation

--- src/variations.ts
+++ src/variations.ts
@@ -1,24 +1,24 @@
-export function getNumberOfChars(name) {
+export function getNumberOfChars(name: string): number {
   // number of characters in: name
-
+  return name.split("").length;
 }
 
-export function getFirstChar(name) {
+export function getFirstChar(name: string): string {
   // first character of: name
-
+  return name[0];
 }
 
-export function getLastChar(name) {
+export function getLastChar(name: string): string {
   // last character of: name
-
+  return name[name.length - 1];
 }
 
-export function getLower(name) {
+export function getLower(name: string): string {
   // name all in lower case (example: "ABC" becomes "abc")
-
+  return name.toLowerCase();
 }
 
-export function getUpper(name) {
+export function getUpper(name: string): string {
   // name all in upper case (example: "abc" becomes "ABC")
-
+  return name.toUpperCase();
 }
